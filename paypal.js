9687fb4f7330634d68fd73a0170677ff1c111d9c
Chris Hsu <26matrices@gmail.com>
2014-06-20 01:48:15 -0400

    Added Extra Features
    Cleaned up some of the displays and added more specifics.

--- paypal.js
+++ paypal.js
@@ -1,10 +1,20 @@
 /** Options **/
 var recurringOptions = [ 10, 25, 50 ];
 var onceOptions = [ 25, 50, 100 ];
+var monthlyOptions = [ 3, 6, 12, 24 ];
+var yearlyOptions = [ 1, 2, 3, 5 ];
 var form = "form#paypal";
 
 /** Helper Functions **/
 function createOptions(options, className) {
+    for (i = 0; i < options.length; i++) {
+        var option = $("<option/>").addClass(className).
+            val(options[i]).text(options[i]);
+        $("#donation_recur select[name=p3] option:last", form).before(option);
+    }
+}
+
+function createAmountOptions(options, className) {
     for (i = 0; i < options.length; i++) {
         var option = $("<option/>").addClass(className).
             val(options[i]).text('$'+options[i]);
@@ -13,22 +23,20 @@ function createOptions(options, className) {
 }
 
 function displayRecurring() {
-    var options = recurringOptions;
     $("input[name=cmd]", form).val("_xclick-subscriptions");
     $("#donation_recur", form).show();
     $(".donation_once", form).remove();
-    createOptions(options, "donation_recurring");
-    $("#donation_amount", form).val(options[0]).
+    createAmountOptions(recurringOptions, "donation_recurring");
+    $("#donation_amount select", form).val(recurringOptions[0]).
         trigger('change').attr('name', 'a3');
 }
 
 function displayOnce() {
-    var options = onceOptions;
     $("input[name=cmd]", form).val("_donations");
     $("#donation_recur", form).hide();
     $(".donation_recurring", form).remove();
-    createOptions(options, "donation_once");
-    $("#donation_amount", form).val(options[0]).
+    createAmountOptions(onceOptions, "donation_once");
+    $("#donation_amount select", form).val(onceOptions[0]).
         trigger('change').attr('name', 'amount');
 }
 
@@ -36,8 +44,9 @@ function displayOnce() {
 $(function() {
     $("#donation_amount", form).hide();
     $("#donation_recur", form).hide();
-    $("#donation_amount_other", form).hide();
+    $("#donation_amount_box", form).hide();
     $("input:radio[name=srt]", form).prop('checked', false);
+    createOptions(monthlyOptions, 'monthly');
     
     $("input:radio[name=srt]", form).change( function() {
         $("#donation_amount", form).show();
@@ -48,32 +57,44 @@ $(function() {
         }
     });
     
-    $("#donation_amount", form).change( function() {
+    $("#donation_amount select", form).change( function() {
         if ($(this).val() == 0) {
             var attr = $(this).attr('name');
-            $("#donation_amount_other", form).show().attr('name', attr);
+            $("#donation_amount_box", form).show();
+            $("#donation_amount_other").attr('name', attr);
             $(this).attr('name', '');
             $("label[for=donation_amount_other]", form).text("");
         } else {
             var attr = $("#donation_amount_other", form).attr('name');
-            $("#donation_amount_other", form).hide().attr('name', '');
+            $("#donation_amount_box", form).hide();
+            $("#donation_amount_other").attr('name', '');
             $(this).attr('name', attr);
             $("label[for=donation_amount_other]", form).text("");
         }
     });
     
+    $("#donation_recur select[name=t3]", form).change( function() {
+        if ($(this).val() == 'M') {
+            $(".yearly", form).remove();
+            createOptions(monthlyOptions, 'monthly');
+        } else {
+            $(".monthly", form).remove();
+            createOptions(yearlyOptions, 'yearly');
+        }
+    });
+    
     $(form).submit( function(e) {
-        if ($("#donation_amount", form).val() == 0) {
+        if ($("#donation_amount select", form).val() == 0) {
             var val = $("#donation_amount_other", form).val();
             if (!isNaN(parseFloat(val)) && isFinite(val)) {
                 return;
             } else {
-                // alert("not a number value!");
                 $("label[for=donation_amount_other]", form).
                     text("Not a number");
                 e.preventDefault();
             }
-        } 
+        }
+        $("input[name=item_name]").val('Shalom Donation - ' + $("#donation_to").val());
         return;
     });
 });
